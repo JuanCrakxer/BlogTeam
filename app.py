9bf106da2b501de3707284c87e1f47ac8388390d
JuanCrakxer <151860566+JuanCrakxer@users.noreply.github.com>
2024-08-29 16:53:21 -0500

    Mi segundo commit

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-from flask import flask, request, render_template, send_file, redirect, url_for
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/Blog', methods=['GET, POST'])
+def Blog():
+    return render_template('Blog.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
